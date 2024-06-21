5687703170f320252b59f9e504877fcbe74a7395
nikita-git <nikita.maximov@gmail.com>
2024-06-21 10:10:19 +0300

    Настройка админки.

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,13 +1,47 @@
+# ice_cream/admin.py
 from django.contrib import admin
 
+from .models import Category, IceCream, Topping, Wrapper
 
-# Register your models here.
-# Из модуля models импортируем модель Category...
-from .models import Category, Topping, Wrapper, IceCream
+admin.site.empty_value_display = 'Не задано'
 
 
-# ...и регистрируем её в админке:
-admin.site.register(Category)
+class IceCreamInline(admin.StackedInline):
+    model = IceCream
+    extra = 0
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = (
+        IceCreamInline,
+    )
+
+
+class IceCreamAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper'
+    )
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category'
+    )
+    search_fields = ('title',)
+    list_filter = ('is_published',)
+    list_display_links = ('title',)
+    filter_horizontal = ('toppings',)
+
+
+# Регистрируем класс с настройками админки для моделей IceCream и Category:
+admin.site.register(IceCream, IceCreamAdmin)
+admin.site.register(Category, CategoryAdmin)
+# Регистрируем модели Topping и Wrapper,
+# чтобы ими можно было управлять через админку
+# (интерфейс админки для этих моделей останется стандартным):
 admin.site.register(Topping)
 admin.site.register(Wrapper)
-admin.site.register(IceCream)
--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -26,7 +26,11 @@ class Topping(PublishedModel):
 
 
 class Wrapper(PublishedModel):
-    title = models.CharField(max_length=256, verbose_name='Название')
+    title = models.CharField(
+        max_length=256,
+        verbose_name='Название',
+        help_text='Уникальное название обёртки, не более 256 символов'
+    )
 
     class Meta:
         verbose_name = 'обёртка'
